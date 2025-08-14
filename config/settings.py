b780764caebb6954616c0e83a057c68a7722e1d9
BoltDreamzz <boltdreamz@gmail.com>
2025-08-14 13:19:54 -0700

    V 1.0

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-80j&#$qepvqq6ld6-69)ek1(42rcs!#*b1yo!!)$95(iqsu19j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['skiiieatz.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://skiiieatz.onrender.com']
 AUTH_USER_MODEL = 'accounts.User'
 
 
